7bcaa6595c3e12f04303eca7e5db385c1d5cdb82
alanexplore <alanexplore@aelfscan.io>
2024-06-30 13:20:43 +0800

    Update CodeUpdatedProcessor

--- src/AelfScan.GenesisApp/Processors/CodeUpdatedProcessor.cs
+++ src/AelfScan.GenesisApp/Processors/CodeUpdatedProcessor.cs
@@ -12,26 +12,7 @@ public class CodeUpdatedProcessor : GenesisProcessorBase<CodeUpdated>
         var id = GetContractInfoId(context.ChainId, logEvent.Address);
 
         var contractInfo = await GetEntityAsync<Entities.ContractInfo>(id);
-        if (contractInfo == null)
-        {
-            if (id == "AELF-pykr77ft9UUKJZLVq15wCH8PinBSjVRQ12sD1Ayq92mKFsJ1i")
-            {
-                contractInfo = new Entities.ContractInfo()
-                {
-                    Address = "pykr77ft9UUKJZLVq15wCH8PinBSjVRQ12sD1Ayq92mKFsJ1i",
-                    CodeHash = "321d000a078787469d1b794cb28bea4ed8c682a7224c0e8fca7067606bb7567a",
-                    Author = "pykr77ft9UUKJZLVq15wCH8PinBSjVRQ12sD1Ayq92mKFsJ1i",
-                    Version = 1,
-                    NameHash = "0000000000000000000000000000000000000000000000000000000000000000",
-                    ContractCategory = 0,
-                    ContractType = ContractType.SystemContract,
-                    Id = "AELF-pykr77ft9UUKJZLVq15wCH8PinBSjVRQ12sD1Ayq92mKFsJ1i"
-                };
-            }
-            
-        }
-
-
+        
         ObjectMapper.Map(logEvent, contractInfo);
         var contractRegistration =
             await GetEntityAsync<ContractRegistration>(
